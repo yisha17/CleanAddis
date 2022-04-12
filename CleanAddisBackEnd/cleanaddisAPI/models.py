add02da264a5606cecd8073b1ab9e3af75e5d8ce
yisha17 <it.yisak.wondim@gmail.com>
2022-04-13 00:37:59 +0300

    separate string and color file

--- CleanAddisBackEnd/cleanaddisAPI/models.py
+++ CleanAddisBackEnd/cleanaddisAPI/models.py
@@ -27,7 +27,7 @@ class User(models.Model):
     username = models.CharField(max_length=20, default="")
     password = models.CharField(max_length=20, default="")
     email = models.EmailField(max_length=30,default="")
-    role = models.CharField(max_length=20, default="")
+    role = models.CharField(max_length=20, default="",null = True)
     profile = models.ImageField(null=True)
     address = models.ForeignKey(Address, on_delete=models.DO_NOTHING,null=True)
     phone = models.CharField(max_length= 20,null= True)
--- clean_addis_android/lib/main.dart
+++ clean_addis_android/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         
         primarySwatch: Colors.blue,
       ),
-      home: NotifyPage(),
+      home: AddWastePage(),
     );
   }
 }
