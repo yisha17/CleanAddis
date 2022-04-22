eb4866d098095ceb6b57f358bda7b9e82e80280e
dathrob <dev.misganaw.meseret@gmail.com>
2022-04-23 00:28:34 +0300

    nav bar

--- clean_addis_desktop/cleanaddis/src/App.js
+++ clean_addis_desktop/cleanaddis/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar';
+//import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar />
+    </>
   );
 }
 
