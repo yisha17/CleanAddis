119dc3d0cac617b76e87ac254b2c0e18a454ec3f
dathrob <dev.misganaw.meseret@gmail.com>
2022-04-23 01:45:14 +0300

    home page

--- clean_addis_desktop/cleanaddis/src/App.js
+++ clean_addis_desktop/cleanaddis/src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import Cleanaddis from './components/Cleanaddis';
 //import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Navbar />,
+      <Cleanaddis />
+      
     </>
   );
 }
