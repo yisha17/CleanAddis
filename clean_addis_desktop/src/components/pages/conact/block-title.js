4a5ffb7e5e6b7227e240aa4edf7e87d5b3fefad3
saron-solomon <61591182+saron-solomon@users.noreply.github.com>
2022-04-23 17:20:13 +0300

    block-title page on progress

---
+++ clean_addis_desktop/src/components/pages/conact/block-title.js
@@ -0,0 +1,7 @@
+import React from "react";
+import heart from "../assets/images/shapes/heart-2-1.png";
+
+const BlockTitle = ({ tagLine, title }) => {
+  return (
+    <div className="block-title">
+      <p></p>
