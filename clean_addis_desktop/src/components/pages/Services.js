a0a013d8b2d11e9aefac349b057751f012fc8e70
saron-solomon <61591182+saron-solomon@users.noreply.github.com>
2022-04-25 13:55:41 +0300

    services edited

--- clean_addis_desktop/src/components/pages/Services.js
+++ clean_addis_desktop/src/components/pages/Services.js
@@ -1,33 +1,12 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-const Services = () => {
-  return ( 
-    <>
-    <Navbar />
-    <div>
-    <div class="bg-gray-100">
-      <section class="cover bg-blue-teal-gradient relative bg-green-500 px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 overflow-hidden py-48 flex
-      items-center min-h-screen">
-        <div class="h-full absolute top-0 left-0 z-0">
-          <img src="images/cover-bg.jpg" alt="" class="w-full h-full object-cover opacity-20"/>
-        </div>
-        <div class="lg:w-3/4 xl:w-2/4 relative z-10 h-100 lg:mt-16">
-          <div>
-            <h1 class="text-white text-4xl md:text-5xl xl:text-6xl font-bold leading-tight">A better life starts with a
-              beautiful
-              smile.</h1>
-              Appointment</a>
+<div class="w-full lg:w-1/2 lg:px-8 mt-12 lg:mt-0">
+          <div class="md:flex">
+            <div>
+              <div class="w-16 h-16 bg-green-500 rounded-full"></div>
+            </div>
+            <div class="md:ml-8 mt-4 md:mt-0">
+              <h4 class="text-xl font-bold leading-tight">Everything You Need Under One Roof</h4>
+              <p class="mt-2 leading-relaxed">Our comprehensive services allow you to receive all needed dental care
+                right here in our state-of-art
+                office – from dental cleanings and fillings to dental implants and extractions.</p>
+            </div>
           </div>
-        </div>
-      </section>
-    </div>
-    <section class="relative px-4 py-16 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 lg:py-32">
-      <div class="flex flex-col lg:flex-row lg:-mx-8">
-        <div class="w-full lg:w-1/2 lg:px-8">
-          <h2 class="text-3xl leading-tight font-bold mt-4">Welcome to the Dentist Office of Dr. Thomas Dooley</h2>
-          <p class="text-lg mt-4 font-semibold">Excellence in Dentistry in the Heart of NY</p>
-          <p class="mt-2 leading-relaxed">Donec convallis sollicitudin facilisis. Integer nisl ligula, accumsan non
-            tincidunt ac, imperdiet in enim.
-            Donec efficitur ullamcorper metus, eu venenatis nunc. Nam eget neque tempus, mollis sem a, faucibus mi.</p>
-        </div>
