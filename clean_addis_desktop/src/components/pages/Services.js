410f943564cc51bb8dd000fab34ccdab83656880
saron-solomon <61591182+saron-solomon@users.noreply.github.com>
2022-04-25 13:56:24 +0300

    services editing

--- clean_addis_desktop/src/components/pages/Services.js
+++ clean_addis_desktop/src/components/pages/Services.js
@@ -10,3 +10,27 @@
                 office – from dental cleanings and fillings to dental implants and extractions.</p>
             </div>
           </div>
+          <div class="md:flex mt-8">
+            <div>
+              <div class="w-16 h-16 bg-green-500 rounded-full"></div>
+            </div>
+            <div class="md:ml-8 mt-4 md:mt-0">
+              <h4 class="text-xl font-bold leading-tight">Our Patient-Focused Approach</h4>
+              <p class="mt-2 leading-relaxed">Your treatment plan will perfectly match your needs, lifestyle, and goals.
+                Even if it’s been years
+                since you last visited the dentist, we can help. Our comfortable office, compassionate team, and
+                minimally-invasive treatments will help you feel completely at ease.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+      <div class="md:flex md:flex-wrap mt-24 text-center md:-mx-4">
+        <div class="md:w-1/2 md:px-4 lg:w-1/4">
+          <div class="bg-white rounded-lg border border-gray-300 p-8">
+            <img src="images/teeth-whitening.svg" alt="" class="h-20 mx-auto"/>
+
+            <h4 class="text-xl font-bold mt-4">Teeth Whitening</h4>
+            <p class="mt-1">Let us show you how our experience.</p>
+            <a href="#" class="block mt-4">Read More</a>
+          </div>
