312258d0cb484e9afe0ac3d716e6fabebd8c54d1
saron-solomon <61591182+saron-solomon@users.noreply.github.com>
2022-04-25 13:57:22 +0300

    service page working

--- clean_addis_desktop/src/components/pages/Services.js
+++ clean_addis_desktop/src/components/pages/Services.js
@@ -1,36 +1,40 @@
-<div class="w-full lg:w-1/2 lg:px-8 mt-12 lg:mt-0">
-          <div class="md:flex">
-            <div>
-              <div class="w-16 h-16 bg-green-500 rounded-full"></div>
-            </div>
-            <div class="md:ml-8 mt-4 md:mt-0">
-              <h4 class="text-xl font-bold leading-tight">Everything You Need Under One Roof</h4>
-              <p class="mt-2 leading-relaxed">Our comprehensive services allow you to receive all needed dental care
-                right here in our state-of-art
-                office – from dental cleanings and fillings to dental implants and extractions.</p>
-            </div>
-          </div>
-          <div class="md:flex mt-8">
-            <div>
-              <div class="w-16 h-16 bg-green-500 rounded-full"></div>
-            </div>
-            <div class="md:ml-8 mt-4 md:mt-0">
-              <h4 class="text-xl font-bold leading-tight">Our Patient-Focused Approach</h4>
-              <p class="mt-2 leading-relaxed">Your treatment plan will perfectly match your needs, lifestyle, and goals.
-                Even if it’s been years
-                since you last visited the dentist, we can help. Our comfortable office, compassionate team, and
-                minimally-invasive treatments will help you feel completely at ease.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    
-      <div class="md:flex md:flex-wrap mt-24 text-center md:-mx-4">
-        <div class="md:w-1/2 md:px-4 lg:w-1/4">
-          <div class="bg-white rounded-lg border border-gray-300 p-8">
-            <img src="images/teeth-whitening.svg" alt="" class="h-20 mx-auto"/>
+</div>
 
-            <h4 class="text-xl font-bold mt-4">Teeth Whitening</h4>
+        <div class="md:w-1/2 md:px-4 mt-4 md:mt-0 lg:w-1/4">
+          <div class="bg-white rounded-lg border border-gray-300 p-8">
+            <img src="images/oral-surgery.svg" alt="" class="h-20 mx-auto"/>
+
+            <h4 class="text-xl font-bold mt-4">Oral Surgery</h4>
             <p class="mt-1">Let us show you how our experience.</p>
             <a href="#" class="block mt-4">Read More</a>
           </div>
+        </div>
+
+        <div class="md:w-1/2 md:px-4 mt-4 md:mt-8 lg:mt-0 lg:w-1/4">
+          <div class="bg-white rounded-lg border border-gray-300 p-8">
+            <img src="images/painless-dentistry.svg" alt="" class="h-20 mx-auto"/>
+
+            <h4 class="text-xl font-bold mt-4">Painless Dentistry</h4>
+            <p class="mt-1">Let us show you how our experience.</p>
+            <a href="#" class="block mt-4">Read More</a>
+          </div>
+        </div>
+
+        <div class="md:w-1/2 md:px-4 mt-4 md:mt-8 lg:mt-0 lg:w-1/4">
+          <div class="bg-white rounded-lg border border-gray-300 p-8">
+            <img src="images/periodontics.svg" alt="" class="h-20 mx-auto"/>
+
+            <h4 class="text-xl font-bold mt-4">Periodontics</h4>
+            <p class="mt-1">Let us show you how our experience.</p>
+            <a href="#" class="block mt-4">Read More</a>
+          </div>
+        </div>
+      </div>
+    </section>
+          </div>
+    <Footer />
+  </>
+  )
+}
+
+export default Services
