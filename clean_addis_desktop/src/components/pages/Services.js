ab89c5138750cadfd52e4e153e27394ef43ff2c1
saron-solomon <61591182+saron-solomon@users.noreply.github.com>
2022-04-25 13:31:13 +0300

    service edited

--- clean_addis_desktop/src/components/pages/Services.js
+++ clean_addis_desktop/src/components/pages/Services.js
@@ -1,3 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+const Services = () => {
+  return ( 
+    <>
+    <Navbar />
+    <div>
+    <div class="bg-gray-100">
+      <section class="cover bg-blue-teal-gradient relative bg-green-500 px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 overflow-hidden py-48 flex
+      items-center min-h-screen">
+        <div class="h-full absolute top-0 left-0 z-0">
+          <img src="images/cover-bg.jpg" alt="" class="w-full h-full object-cover opacity-20"/>
+        </div>
