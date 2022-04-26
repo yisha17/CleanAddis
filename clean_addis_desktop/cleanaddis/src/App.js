d080963605a91055168174d2a4138b8198015c9e
dathrob <dev.misganaw.meseret@gmail.com>
2022-04-26 16:16:42 +0300

    admin panel start up files

--- clean_addis_desktop/cleanaddis/src/App.js
+++ clean_addis_desktop/cleanaddis/src/App.js
@@ -8,16 +8,21 @@ import { Routes } from 'react-router-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
+import Home from './pages/adminpages/home/Home';
 function App() {
   
   return (
   
-    <>
+    /*<>
       <Navbar />
       <Cleanaddis />
       <Hero />
       <Footer />
-    </>
+    </>*/
+    <div className="App">
+      <Home/>
+
+    </div>
   );
 }
 
