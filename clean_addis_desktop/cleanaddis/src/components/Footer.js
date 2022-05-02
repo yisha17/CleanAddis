139352bacf76a4cfe8522f17ccb3c46f3c16516b
dathrob <dev.misganaw.meseret@gmail.com>
2022-05-02 15:53:24 +0300

    fix conflict

--- clean_addis_desktop/cleanaddis/src/components/Footer.js
+++ clean_addis_desktop/cleanaddis/src/components/Footer.js
@@ -3,7 +3,7 @@ import CleanAddis from './CleanAddis.jpg'
 const Footer = () => {
   return (
     <footer class="p-4 bg-white sm:p-6 dark:bg-gray-800 bottom-0">
-    <div class="md:flex md:justify-between mt-10">
+    <div class="md:flex md:justify-between mt-20 mb-0">
         <div class="mb-6 md:mb-0">
             <a href="/" class="flex items-center">
                 <img src={CleanAddis} class="mr-3 h-40" alt="CleanAddis Logo" />           </a>
@@ -44,8 +44,8 @@ const Footer = () => {
             </div>
         </div>
     </div>
-    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-    <div class="sm:flex sm:items-center sm:justify-between">
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8 " />
+    <div class="sm:flex sm:items-center sm:justify-between mt-2">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" class="hover:underline">CleanAddis ™</a>. All Rights Reserved.
         </span>
         <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
