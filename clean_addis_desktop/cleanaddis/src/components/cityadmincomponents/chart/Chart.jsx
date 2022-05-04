ec262422d7b5b08e1c26b38012373ef0d7b50dcf
dathrob <dev.misganaw.meseret@gmail.com>
2022-05-04 11:30:14 +0300

    admin dashboard chart

--- clean_addis_desktop/cleanaddis/src/components/cityadmincomponents/chart/Chart.jsx
+++ clean_addis_desktop/cleanaddis/src/components/cityadmincomponents/chart/Chart.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
 import "./chart.scss"
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip , ResponsiveContainer } from 'recharts';
+const data = [
+  {name:'january',amount:'5000'},
+  {name:'february',amount:'3000'},
+  {name:'march',amount:'2000'},
+  {name:'april',amount:'4050'},
 
-
+];
 const Chart = () => {
   return (
-    <div className="chart">
-      Chart
+    <div className="chart flex flex-col">
+      <div className='title self-center'>Last 6 month performance</div>
+      <ResponsiveContainer width="100%" height="100%">
+      <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="amount" stackId="1" stroke="#8884d8" fill="lightgreen" />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
   )
 }
