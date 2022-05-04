abae124dcdeddad648e0fe13a4e8b2785a678ca5
dathrob <dev.misganaw.meseret@gmail.com>
2022-05-04 14:15:47 +0300

    data table started

--- clean_addis_desktop/cleanaddis/src/pages/adminpages/list/List.jsx
+++ clean_addis_desktop/cleanaddis/src/pages/adminpages/list/List.jsx
@@ -1,10 +1,16 @@
 import "./list.scss"
-
+import CityadminSidebar from "../../../components/cityadmincomponents/sidebar/CityadminSidebar"
+import CityadminNavbar from "../../../components/cityadmincomponents/navbar/CityadminNavbar"
+import Datatable from "../../../components/cityadmincomponents/datatable/Datatable"
 
 const List = () => {
   return (
-    <div>
-      
+    <div className="list flex">
+    <CityadminSidebar />
+    <div className="listContainer ">
+      <CityadminNavbar />
+      <Datatable />
+    </div>
     </div>
   )
 }
