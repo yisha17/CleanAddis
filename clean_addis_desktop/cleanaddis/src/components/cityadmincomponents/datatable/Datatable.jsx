c4a39bc55a048598505df96b0532bda51e143f87
dathrob <dev.misganaw.meseret@gmail.com>
2022-05-04 20:39:38 +0300

    add custome action buttons

--- clean_addis_desktop/cleanaddis/src/components/cityadmincomponents/datatable/Datatable.jsx
+++ clean_addis_desktop/cleanaddis/src/components/cityadmincomponents/datatable/Datatable.jsx
@@ -5,11 +5,21 @@ import { userColumns, userRows } from '../../../datatablesource'
 
 
 const Datatable = () => {
+    const actionColumn = [{field:"action", headerName:"Action", width:230,
+    renderCell:(params) => {
+     return(
+         <div className = "cellAction flex gap-4">
+           <div className="viewButton border rounded border-slate-300 p-1 hover:bg-blue-400 cursor-pointer">view</div> 
+           <div className="deleteButton border rounded border-slate-300 p-1 hover:bg-slate-500 cursor-pointer ">delete</div>
+         </div>
+     )   
+    }},]
+
   return (
     <div  style={{ height: 400, width: '100%' }} className="items-center">
        <DataGrid 
         rows={userRows}
-        columns={userColumns}
+        columns={userColumns.concat(actionColumn)}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
