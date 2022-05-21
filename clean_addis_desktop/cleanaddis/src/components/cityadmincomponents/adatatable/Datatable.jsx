2214abcfc9f925a652517bb4f2c56e5e72df703b
dathrob <dev.misganaw.meseret@gmail.com>
2022-05-21 17:16:56 +0300

    announcement modal started

--- clean_addis_desktop/cleanaddis/src/components/cityadmincomponents/adatatable/Datatable.jsx
+++ clean_addis_desktop/cleanaddis/src/components/cityadmincomponents/adatatable/Datatable.jsx
@@ -4,7 +4,8 @@ import { DataGrid } from '@mui/x-data-grid'
 import { userColumns, userRows } from '../../../datatablesource'
 import {Link} from 'react-router-dom'
 import New from '../../../pages/adminpages/new/New'
-
+import { useState } from 'react'
+import Modal from "../../../components/cityadmincomponents/rdatatable/Modal"
 
 const Adatatable = () => {
     const actionColumn = [{field:"action", headerName:"Action", width:230,
@@ -19,9 +20,15 @@ const Adatatable = () => {
      )   
     }},]
 
+    
+
   return (
     <div>
-  
+      <div>
+      <Link to="1">
+           <div className="border rounded border-slate-300 justify-center flex items-center m-4  hover:bg-green-400 cursor-pointer pt-4 pb-5">Add New Announcement </div> 
+           </Link>
+      </div>
     <div  style={{ height: 500, width: '100%' }} className="items-center">
        <DataGrid 
         rows={userRows}
