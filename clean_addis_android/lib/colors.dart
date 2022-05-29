341f1751d184db94a43655cff405df856911aae7
Avstel <it.nathan.demissie@gmail.com>
2022-05-29 17:57:50 +0300

    Colors updated double

--- clean_addis_android/lib/colors.dart
+++ clean_addis_android/lib/colors.dart
@@ -1,2 +1,4 @@
 Color darkgreen = Color(0xffe9fff3);
-Color logicalgreen = Color(0xff68EA26);
+Color logicalgreen = Color(0xff68A26);
+Color darkgreen = Color(0xf3e1f11f);
+Color logicalgreen = Color(0xfe8926);
